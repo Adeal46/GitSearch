e76283cf93b61e506238c7ee645bb1d1d493ec81
Adeal46 <adililema3@gmail.com>
2018-11-30 22:17:46 +0300

    added background image for page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'a9dd3bf1dbfcc066749d59eceea76343f813b04b'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
