3fef2fc26da6ccf98ba4340fee11b9111f4055dc
Adeal46 <adililema3@gmail.com>
2018-11-30 22:19:50 +0300

    added user display repo

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { GithubComponent } from '../github/github.component';
+import { RepositoriesComponent } from '../repositories/repositories.component';
+
+const routes: Routes = [
+  { path: 'users', component: GithubComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+
+
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [
+    GithubComponent,
+    RepositoriesComponent,
+  ],
+  exports: [RouterModule],
+})
+export class RoutingModule { }
