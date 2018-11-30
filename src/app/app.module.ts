7e2f2b562aad62918c310aaceb26cb93a6cb38c8
Adeal46 <adililema3@gmail.com>
2018-11-30 22:09:20 +0300

    added classes and components for use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 import { AppComponent } from './app.component';
+import {RoutingModule} from './routing/routing.module';
+import { RouterModule } from '@angular/router';
+import { SearchFormComponent } from './search-form/search-form.component';
+import {FormsModule} from '@angular/forms';
+import {SearchRequestService} from './search-request.service';
+import {MatMenuModule} from '@angular/material/menu';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RoutingModule,
+      RouterModule,
+      FormsModule,
+      NgProgressModule.forRoot(),
+      NgProgressHttpModule.forRoot(),
+      HttpClientModule
   ],
-  providers: [],
+  providers: [SearchRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
